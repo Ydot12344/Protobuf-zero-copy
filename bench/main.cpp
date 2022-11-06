039661318d4cac897464b7560a794181bcd92630
Ydot <andreizdor@yandex.ru>
2022-11-06 21:53:23 +0300

    add equal checks to bench

--- bench/main.cpp
+++ bench/main.cpp
@@ -1,5 +1,6 @@
 #include "messge.pb.h"
 #include <benchmark/benchmark.h>
+#include <gtest/gtest.h>
 #include <gen_data.h>
 #include <fstream>
 
@@ -29,7 +30,6 @@ public:
     TEnvHolder() {
         // Init
 
-        NGenProto::TGenOpts opts;
         opts.NumsCount = 100;
         opts.StringsCount = 100;
         opts.StringSize = 100;
@@ -43,12 +43,69 @@ public:
         report.SerializeToString(&out);
     }
 
+    NGenProto::TGenOpts opts;
     tutorial::TReport report;
 };
+
+void CheckFilesEqual(const tutorial::TFile& a, const tutorial::TFile& b) {
+    ASSERT_EQ(a.name(), b.name());
+    ASSERT_EQ(a.weights_size(), b.weights_size());
+    ASSERT_EQ(a.hosts_size(), b.hosts_size());
+
+    for (size_t i = 0; i < a.weights_size(); i++) {
+        ASSERT_EQ(a.weights(i), b.weights(i));
+    }
+
+    for (size_t i = 0; i < a.hosts_size(); i++) {
+        ASSERT_EQ(a.hosts(i), b.hosts(i));
+    }
+}
+
+void CheckSetsOfFilesEqual(const tutorial::TFileSet& a, const tutorial::TFileSet& b) {
+    ASSERT_EQ(a.hash(), b.hash());
+
+    ASSERT_EQ(a.files_size(), b.files_size());
+
+    for (size_t i = 0; i < a.files_size(); i++) {
+        CheckFilesEqual(a.files(i), b.files(i));
+    }
+}
+
+void Check(const std::string& resultBin) {
+    const auto& env = *TSingletone<TEnvHolder>();
+
+    tutorial::TReport expected = env->report;
+    tutorial::TReport result;
+
+    result.ParseFromString(resultBin);
+
+    ASSERT_EQ(result.nums_size(), 0);
+    ASSERT_EQ(result.strings_size(), 0);
+    ASSERT_EQ(result.setsoffiles_size(), env->opts.SetsOfFilesCount / 2);
+
+    std::vector<tutorial::TFileSet> sets;
+
+    for (size_t i = 0; i < expected.setsoffiles_size(); i++) {
+        sets.push_back(expected.setsoffiles(i));
+    }
+
+    std::sort(sets.begin(), sets.end(), 
+        [](const auto& a, const auto& b) {
+            return a.hash() > b.hash();
+        }
+    );
+
+    for (size_t i = 0; i < result.setsoffiles_size(); i++) {
+        CheckSetsOfFilesEqual(sets[i], result.setsoffiles(i));
+    }
+}
 
 static void BM_ParseProtoFromFile(benchmark::State& state) {
 
     const auto& env = *TSingletone<TEnvHolder>();
+    std::string out;
+    out.reserve(170000000);
+
     for (auto _ : state) {
         std::ifstream in("out.txt");
 
@@ -65,10 +122,10 @@ static void BM_ParseProtoFromFile(benchmark::State& state) {
             *res.add_setsoffiles() = a.setsoffiles(i);
         }
 
-        std::ofstream out("res.txt");
-        res.SerializeToOstream(&out);
+        res.SerializeToString(&out);
     }
     
+    Check(out);
 }
 // Register the function as a benchmark
 BENCHMARK(BM_ParseProtoFromFile);
--- flame_from_bench.sh
+++ flame_from_bench.sh
@@ -1,3 +1,3 @@
-sudo perf record -g ./build/bench/bench 
+sudo perf record -D3067 -g ./build/bench/bench 
 sudo perf script > perf.script
 ./FlameGraph/stackcollapse-perf.pl perf.script | ./FlameGraph/flamegraph.pl > flamegraph.svg
